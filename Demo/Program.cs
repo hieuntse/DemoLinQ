a1a42a525f6f707802fc203faa4c21c779c7c806
hieuntse <hieusedn@gmail.com>
2024-05-21 16:57:32 +0700

    demo

---
+++ Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Program
+{
+    public delegate void NavigatorTank(int id);
+    public event NavigatorTank NavigateTank;
+
+    public void OnNavigatorTank(int id)
+    {
+        NavigateTank?.Invoke(id);
+    }
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+
+        // Đăng ký một phương thức xử lý sự kiện
+        //program.NavigateTank += HandleEvolveTank;
+        //program.NavigateTank += HandleUpgradeTank;
+        Console.WriteLine();
+        Console.WriteLine("Huy event HandleEvolveTank");
+        program.NavigateTank += HandleEvolveTank;
+        program.OnNavigatorTank(1);
+        program.NavigateTank += HandleUpgradeTank;
+        program.OnNavigatorTank(2);
+        Console.WriteLine();
+        Console.WriteLine("Huy event HandleUpgradeTank");
+        program.NavigateTank += HandleMoveTank;
+        program.OnNavigatorTank(3);
+        program.NavigateTank += HandleKillTank;
+        program.OnNavigatorTank(4);
+
+
+
+        // Gọi phương thức OnNavigatorTank, sẽ kích hoạt sự kiện NavigateTank
+        // program.OnNavigatorTank(4);
+    }
+
+    static void HandleEvolveTank(int id)
+    {
+        Console.WriteLine($"Da tien hoa tank co id la : {id}");
+    }
+    static void HandleUpgradeTank(int id)
+    {
+        Console.WriteLine($"Da nang cap tank co id la {id}");
+    }
+    static void HandleMoveTank(int id)
+    {
+        Console.WriteLine($"Da di chuyen tank co id la : {id}");
+    }
+    static void HandleKillTank(int id)
+    {
+        Console.WriteLine($"Da kill tank co id la {id}");
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Lesson4
+{
+    public delegate void TangQuaDelegate(string gift);
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person vo = new Person("Bup be");
+            Program program = new Program();
+            program.oNha(vo, program.tangQua);
+            program.dlg("nguoi yeu");
+            program.lamdaDlg(" vo");
+
+
+        }
+
+        public void tangQua(string qua)
+        {
+            Console.WriteLine("Da tang " + qua);
+        }
+        //public void oNha(Person friend, tangQua)
+        //{
+        //    var qua = "Quà đã nhận";
+        //    tangQua(qua);
+        //}
+
+        //TangQuaDelegate tangQua = tangQua() - Truyen func vao tham so/ k phai thuc thi nen bo ();
+        public void oNha(Person friend, TangQuaDelegate tangQua)
+        {
+            var qua = "Qua da nhan tu " + friend.Name;
+            tangQua(qua);
+        }
+        TangQuaDelegate dlg =
+          delegate (string qua) { Console.WriteLine("Tang qua lan 2 la " + qua); };
+        //Dùng lambda expression
+        TangQuaDelegate lamdaDlg = (qua) => { Console.WriteLine("Tang qua lan 3 la" + qua); };
+
+        //Câu lệnh đầy đủ của lambda expression.
+        //Dấu "=>" gọi là go-to
+        //(parameters) => { statement
+    }
+
+}
